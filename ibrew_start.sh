163de8f292e852d31e887571ab52ecec06fd96af
Toby <toby@idodev.co.uk>
2017-03-08 00:47:15 +0000

    updated dockerfile to pass ip

--- ibrew_start.sh
+++ ibrew_start.sh
@@ -3,4 +3,4 @@
 docker kill ibrew
 docker rm ibrew
 docker pull idodev/ibrew-api
-docker run --name=ibrew -d --restart=always --net=host -p 3000:3000  idodev/ibrew-api
+docker run --name=ibrew -d --restart=always --net=host -p 3000:3000 -e IKETTLE_IP=192.168.0.34 idodev/ibrew-api
